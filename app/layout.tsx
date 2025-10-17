3b8d1a0225b14558af86525314b963fe769a5d1c
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2025-10-17 07:18:05 +0700

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +61,7 @@ export default function RootLayout({
         />
         <p className="ml-4 mt-4 md:mt-0 text-sm md:text-md lg:text-lg opacity-45 font-semibold">© Departemen IPTEK Himpunan Mahasiswa Teknik Telekomunikasi Kabinet Arunika Cipta</p>
       </footer>
+      <Analytics />
       </body>
     </html>
   );
