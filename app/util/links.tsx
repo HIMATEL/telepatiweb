0ac69990650997149f1b8abddced66504a4ac8fd
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2025-10-18 17:45:51 +0700

    fix link

--- app/util/links.tsx
+++ app/util/links.tsx
@@ -1,7 +1,7 @@
 export const links = {
    pendaftaran: "https://docs.google.com/forms/d/e/1FAIpQLSdWFVY3_G6Qdm20CQik86opUMR3Qir2H0E69Vt0oUGhW-X6cw/viewform",
    proposalKreatif: "https://www.canva.com/design/DAGyqoWA25I/Y7oKcrXJ_FmnrZWOw2-mQg/view?utm_content=DAGyqoWA25I&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=ha2c21f38c2",
-   guidebook: "https://bit.ly/GUIDEBOOKTELEPATI2025"
+   guidebook: "https://www.canva.com/design/DAGzUQNWlYY/poG_Y-pDeeP93M06Hi6VsQ/view?utm_content=DAGzUQNWlYY&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h584def43e3"
 }
 
 export const GOOGLE_DRIVE_BASE_URL = "https://lh3.googleusercontent.com/d/";
